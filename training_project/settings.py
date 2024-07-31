4b527a6d37d3ab560155899c2239cba94ae159b4
sidi ahmed <sidiahmedhabib@gmail.com>
2024-07-31 17:41:32 +0000

    finish hello world chapter

--- training_project/settings.py
+++ training_project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'training_app.apps.TrainingAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
