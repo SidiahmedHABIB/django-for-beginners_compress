5a89f6417cdda0bbd0ec7bb5e39637762939fa0c
sidi ahmed <sidiahmedhabib@gmail.com>
2024-08-01 22:10:53 +0000

    finish chap 9

--- training_app/views.py
+++ training_app/views.py
@@ -1,7 +1,12 @@
 from django.views.generic import DeleteView, UpdateView,TemplateView,CreateView ,ListView,DetailView
 from .models import Post , Blog 
 from django.urls import reverse_lazy # new
+from .forms import CustomUserCreationForm
 
+class SignUpView(CreateView):
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'signup.html'
 class HomePageView(ListView):
     model = Post
     template_name = 'home.html'
