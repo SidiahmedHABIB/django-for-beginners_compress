77bfe5a18ac98bff32b9542b959ad5fe683dfb5c
sidi ahmed <sidiahmedhabib@gmail.com>
2024-08-01 09:47:52 +0000

    finish chap four

--- training_app/views.py
+++ training_app/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import HttpResponse
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView ,ListView
+from .models import Post
 
-class HomePageView(TemplateView):
-    template_name = 'pages/home.html'
+class HomePageView(ListView):
+    model = Post
+    template_name = 'home.html'
+    context_object_name = 'all_posts_list' # new
+
 
 class AboutPageView(TemplateView):
-    template_name = 'pages/about.html'
+    template_name = 'about.html'
